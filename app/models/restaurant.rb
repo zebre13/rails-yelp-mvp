5a1ebb5aaa60e6c3a35c44e8d0441c2afd033470
Boris <95616326+zebre13@users.noreply.github.com>
2022-05-27 10:27:41 +0200

    models ok

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,5 +2,5 @@ class Restaurant < ApplicationRecord
   has_many :reviews, dependent: :destroy
 
   validates :name, :address, :category, presence: true
-  validates :category, inclusion: { in: %w[chinese italian japanese french belgian], message: '%{value} is not a valid category' }
+  validates :category, inclusion: { in: %w[chinese italian japanese french belgian], message: '%<value> is not a valid category' }
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -2,5 +2,5 @@ class Review < ApplicationRecord
   belongs_to :restaurant
 
   validates :rating, :content, presence: true, allow_blank: false
-  validates :rating, inclusion: [0, 1, 2, 3, 4, 5], numericality: { only_integer: true }
+  validates :rating, inclusion: { in: [0, 1, 2, 3, 4, 5] }, numericality: { only_integer: true }
 end

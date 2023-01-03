df39fd3f036a20ab99b4edb305d86b41de968665
Boris <95616326+zebre13@users.noreply.github.com>
2023-01-03 12:15:39 +0100

    generated seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,40 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+royal_dragon = {
+  name: 'Royal Dragon',
+  address: '98 boulevard De Montparnasse, 75014 Paris France',
+  phone_number: '+33 1 43 35 37 30',
+  category: 'chinese'
+}
+pizza_caratello = {
+  name: 'Pizza Caratello',
+  address: '8 rue Audran, 75018 Paris France',
+  phone_number: '+33 1 42 64 88 51',
+  category: 'italian'
+}
+mian_fan = {
+  name: 'Mian Fan',
+  address: '15 boulevard Montmartre, 75002 Paris France',
+  phone_number: '+33 9 67 47 47 06',
+  category: 'japanese'
+}
+a_la_chataigne = {
+  name: 'A La Chataigne',
+  address: '22 rue de Miromesnil, 75008 Paris France',
+  phone_number: '+33 1 40 07 90 86',
+  category: 'french'
+}
+au_rendez_vous_des_belges = {
+  name: 'Au Rendez-Vous des Belges',
+  address: '23 rue de Dunkerque, 75010 Paris France',
+  phone_number: '+33 1 42 82 04 72',
+  category: 'belgian'
+}
+
+[royal_dragon, pizza_caratello, mian_fan, a_la_chataigne, au_rendez_vous_des_belges].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts 'Finished!'

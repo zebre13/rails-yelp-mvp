27b7b1c31b4a5d6b08871b424061132a629f5e0e
Boris <95616326+zebre13@users.noreply.github.com>
2023-01-03 13:56:07 +0100

    reviews wip

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :restaurants do
-    resources :reviews
+  resources :restaurants, only: %i[index show new create] do
+    resources :reviews, only: %i[new create]
   end
 end
